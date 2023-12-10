48509937ce031ef6cde5e6a32186c1828bb60a52
Amit Rajwar <74517134+amitrajwar669@users.noreply.github.com>
2023-12-10 23:34:41 +0530

    Add files via upload

---
+++ Java/JAVA OPPS/Cunstructor/Employee.java
@@ -0,0 +1,70 @@
+               ///CONSTRUCTOR////
+               
+       //default CONSTRUCTOR///        
+// class Test
+// {
+//   int i;
+//   public static void main(String[] args)
+//   {
+//     Test t = new Test();
+//     System.out.println(t.i);
+//   }
+// }
+
+      //no-arg CONSTRUCTOR//
+
+
+// class Test
+// {
+//   Test()
+//   {
+//     System.out.println("i am no-arg CONSTRUCTOR");
+//   }
+//   public static void main(String[] args)
+//   {
+//     Test t = new Test();
+    
+//   }
+// }
+
+    // parametrized CONSTRUCTOR//
+    
+// class Test
+// {
+//   Test(int a)
+//   {
+//     System.out.println("parametrized CONSTRUCTOR");
+//   }
+//   public static void main(String[] args)
+//   {
+//     Test t = new Test(10);
+    
+//   }
+// }
+
+
+
+
+
+/////////////////////////////////////////
+
+
+
+
+// class Employee
+// {
+//   }String name;
+//   int emp_id;
+  
+//   Employee(String name, int emp_id)
+//   {
+//     this.name=name;
+//     this.emp_id = emp_id;
+//   }
+//   public static void main(String[] args)
+//   {
+//     Employee e1 = new Employee("deepak",10);
+//     Employee e2 = new Employee("sbc", 20);
+//     System.out.println("Employee 1 : "+e1.name+" "+e1.emp_id);
+//     System.out.println("Employee 2 : "+e2.name+" "+e2.emp_id);
+//   }
