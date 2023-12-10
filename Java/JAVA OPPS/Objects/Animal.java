e4ce0eec565d8b33f7c3b46577c8dbdc84f76602
Amit Rajwar <74517134+amitrajwar669@users.noreply.github.com>
2023-12-10 23:26:40 +0530

    object

---
+++ Java/JAVA OPPS/Objects/Animal.java
@@ -0,0 +1,67 @@
+
+//     //  Object 
+// class Animal//Class
+// {
+//     public void eat() //Methods
+//     {
+//         System.out.println("I Am Eating");
+//     }
+//     public static void main (String[] args){//main method
+//         System.out.println("1");
+//         Animal bujo = new Animal();//obj
+//         bujo.eat();
+//         bujo.run();
+//         Bird buzo1= new Bird();
+//         buzo1.fly();
+//     }
+//     public void run()
+//     {
+//         System.out.println("I am Running");
+//     }
+// }
+// class Bird
+// {
+//   public void fly()
+//   {
+//     System.out.println("I am flying");
+//   }
+// }
+        // ------>///By Refrance Variable////----->
+
+// class Animal
+// {
+//   String color;
+//   int age;
+//   public static void main(String[] args)
+//   {
+//     Animal buzo = new Animal();
+//     buzo.color="Black";
+//     buzo.age=10;
+//     System.out.println(buzo.color+" "+buzo.age);
+//   }
+// }
+//           ----->Using Methods<-------
+           
+           
+           
+// class Animal
+// {
+//   String color;
+//   int age;
+//   void intObj(String c, int a)
+//   {
+//     color=c;
+//     age=a;
+//   }
+//   void display()
+//   {
+//     System.out.println(color+" "+age);
+//   }
+//   public static void main(String[] args)
+//   {
+//     Animal buzo = new Animal();
+//     buzo.intObj("black",10);
+//     buzo.display();
+//   }
+// }
+     
